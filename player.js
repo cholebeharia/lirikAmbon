cca9e8e8cfdf8eb61dd8ec2ae5db57f645dc2534
Usman Taufiq Lebeharia <cholebeharia@gmail.com>
2018-02-16 02:25:17 +0900

    Update player.js

--- player.js
+++ player.js
@@ -12,35 +12,35 @@ var ap1 = new APlayer({
         author: 'Marvey Kaya',
         url: 'https://sites.google.com/site/ambonmp3/mp3/Marvel%20Kaya%20-%20Paleng%20Bae.mp3',
         pic: 'https://sites.google.com/site/ambonmp3/gambarmp3/mk-palengbae.jpg',
-		lrc: 'https://rawgit.com/cholebeharia/lirikAmbon/master/mk-palengbae.txt'
+	lrc: 'https://rawgit.com/cholebeharia/lirikAmbon/master/mk-palengbae.txt'
     },
 	{
-		title: 'Mama',
+	title: 'Mama',
         author: 'Marvey Kaya',
         url: 'https://sites.google.com/site/ambonmp3/mp3/Marvel%20Kaya%20-%20Mama.mp3',
         pic: 'https://sites.google.com/site/ambonmp3/gambarmp3/mk-mama.jpg',
-		lrc: 'https://rawgit.com/cholebeharia/lirikAmbon/master/Marvey%20Kaya%20-%20Mama.txt'
+	lrc: 'https://rawgit.com/cholebeharia/lirikAmbon/master/Marvey%20Kaya%20-%20Mama.txt'
 	},
 	{
-		title: 'Beta Harap',
+	title: 'Beta Harap',
         author: 'Mitha Talahatu',
         url: 'https://sites.google.com/site/ambonmp3/mp3/amq%20beta%20harap%20-mitha.mp3',
         pic: 'https://sites.google.com/site/ambonmp3/gambarmp3/mt-betaharap.jpg',
-		lrc: 'https://rawgit.com/cholebeharia/lirikAmbon/master/Mitha%20BetaHarap.txt'
+	lrc: 'https://rawgit.com/cholebeharia/lirikAmbon/master/Mitha%20BetaHarap.txt'
 	},
 	{
-		title: 'Mati Rasa',
+	title: 'Mati Rasa',
         author: 'Richard Rehatta',
         url: 'https://sites.google.com/site/ambonmp3/mp3/Richard%20Rehatta%20-%20Mati%20Rasa.mp3',
         pic: 'https://sites.google.com/site/ambonmp3/gambarmp3/rr-betamatirasa.jpg',
-		lrc: 'https://rawgit.com/cholebeharia/lirikAmbon/master/Richard%20Rehatta%20-%20Mati%20Rasa.txt'
+	lrc: 'https://rawgit.com/cholebeharia/lirikAmbon/master/Richard%20Rehatta%20-%20Mati%20Rasa.txt'
 	},
 	{
-		title: 'Jang Dengar',
+	title: 'Jang Dengar',
         author: 'Youchen Amos',
         url: 'https://sites.google.com/site/ambonmp3/mp3/Youchen%20Amos%20-%20Jang%20Dengar.mp3',
         pic: 'https://sites.google.com/site/ambonmp3/gambarmp3/yo-jangdengar.jpg',
-		lrc: 'https://rawgit.com/cholebeharia/lirikAmbon/master/Youchen%20Amos%20-%20Jang%20Dengar.txt'
+	lrc: 'https://rawgit.com/cholebeharia/lirikAmbon/master/Youchen%20Amos%20-%20Jang%20Dengar.txt'
 	}
 	]
 });
